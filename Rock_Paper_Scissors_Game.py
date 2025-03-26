45b35d78158af036cd6b5e99f0e3d4af7dd101d4
nrminaaa <abdullayevanermine141@gmail.com>
2025-03-26 20:50:00 +0400

    yoxlamasizdir

--- Rock_Paper_Scissors_Game.py
+++ Rock_Paper_Scissors_Game.py
@@ -1,50 +1,43 @@
 score1=0
 score2=0
-while True :
+i=0
+while i<3:
+    i=i+1
+    print("please enter the rock,paper or sciosser")
     player1=input()
-    if player1 not in ["rock,paper,sciosser"]:
-        print("please enter the rock,paper,sciosser")
-        player1=input()
-
     player2=input()
-    if player2 not in ["rock,paper,sciosser"]:
-        print("please enter the rock,paper,sciosser")
-        player2=input()
-
-        break
-
-    for i in range(0,4):
-   
-        if player1=="rock" and player2=="rock":
+    if player1=="rock" and player2=="rock":
          print("there is no winner")
-        elif player1=="rock" and player2=="paper":
+    elif player1=="rock" and player2=="paper":
          print("the winner is player2")
          score2=score2+1
-        elif player1=="rock" and player2=="sciosser":
+    elif player1=="rock" and player2=="sciosser":
          print("the winner is player1")
          score1=score1+1
-        elif player1=="paper" and player2=="rock":
+    elif player1=="paper" and player2=="rock":
           print("the winner is player1")
           score1=score1+1
-        elif player1=="paper" and player2=="paper":
+    elif player1=="paper" and player2=="paper":
          print("there is no winner")
-        elif player1=="paper" and player2=="sciosser":
+    elif player1=="paper" and player2=="sciosser":
          print("the winner is player2")
          score2=score2+1
-        elif player1=="sciosser" and player2=="rock":
+    elif player1=="sciosser" and player2=="rock":
           print("the winner is player2")
           score2=score2+1
-        elif player1=="sciosser" and player2=="paper":
+    elif player1=="sciosser" and player2=="paper":
           print("the winner is player1")
           score1=score1+1
-        elif player1=="sciosser" and player2=="sciosser":
+    elif player1=="sciosser" and player2=="sciosser":
           print("there is no winner")
 
+
+        
 if score1>score2:
-    print("THE WINNER PLAYER 1")
+    print (score1,":",score2, "THE WINNER PLAYER")
 elif score1<score2:
-    print("THE WINNER PLAYER 2")
+    print(score2,":",score1,"THE WINNER PLAYER 2")
 else:
-    print("THERE IS NOT WINNER")
+    print( score2,":",score1,"THERE IS NOT WINNER")
 
 
